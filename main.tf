2b1ba34863cc077bb19edff7d1ad5957c5511a63
Curtis Rubeck <rubeckcurtis@gmail.com>
2021-09-30 20:10:35 -0500

    created providers and variables config files

--- main.tf
+++ main.tf
@@ -1,26 +1,11 @@
-# Provider.tf
-terraform {
-  required_version = "~> 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-2"
-
-  default_tags {
-    tags = {
-      Environment = "Test"
-      Terraform = true
-    }
-  }
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "dev-test"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr_block" {
+  description = "VPC cidr block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
